2063b1cc3b474129b14cf4e670325964613194f8
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-10-17 17:30:19 -0400

    Agregar manejador de usuario con constructor para el servicio de usuario

---
+++ internal/handlers/user.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"SubsTracker/internal/service"
+)
+
+type UserHandler struct {
+	service *service.UserService
+}
+
+func NewUserHandler(service *service.UserService) *UserHandler {
+	return &UserHandler{
+		service: service,
+	}
+}
+
